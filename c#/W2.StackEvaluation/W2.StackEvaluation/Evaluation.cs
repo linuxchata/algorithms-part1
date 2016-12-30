359f0ea19cf6e5275f47c08398ebf364288fa183
LinuxChata <linuxchata@hotmail.com>
2016-12-30 18:29:32 +0100

    Evaluation stack

---
+++ c#/W2.StackEvaluation/W2.StackEvaluation/Evaluation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W2.StackEvaluation
+{
+    public class Evaluation
+    {
+        private Stack<int> digits = new Stack<int>();
+        private Stack<char> operations = new Stack<char>();
+
+        public int Evaluate(string eval)
+        {
+            var chars = eval.ToCharArray();
+
+            foreach (var item in chars)
+            {
+                if (Char.IsDigit(item))
+                {
+                    this.digits.Push((int)Char.GetNumericValue(item));
+                }
+                else if (item == '+' || item == '*')
+                {
+                    this.operations.Push(item);
+                }
+                else if (item == ')')
+                {
+                    int value = 0;
+                    var operation = this.operations.Pop();
+                    if (operation == '+')
+                    {
+                        value = this.digits.Pop() + this.digits.Pop();
+                    }
+                    else if (operation == '*')
+                    {
+                        value = this.digits.Pop() * this.digits.Pop();
+                    }
+
+                    this.digits.Push(value);
+                }
+            }
+
+            return this.digits.Pop();
+        }
+    }
+}
---
+++ c#/W2.StackEvaluation/W2.StackEvaluation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace W2.StackEvaluation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = "(1 + ((2 + 3) * (4 * 5)))";
+
+            var eval = new Evaluation();
+            Console.WriteLine(eval.Evaluate(input));
+
+            Console.ReadKey();
+        }
+    }
+}
