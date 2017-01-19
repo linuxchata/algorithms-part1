76fa2843caed05dc187275236dc5d2ac80158c55
LinuxChata <linuxchata@hotmail.com>
2017-01-19 22:36:28 +0100

    BST: Base project

---
+++ c#/W4.BinarySearchTrees/W4.BinarySearchTrees/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace W4.BinarySearchTrees
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
