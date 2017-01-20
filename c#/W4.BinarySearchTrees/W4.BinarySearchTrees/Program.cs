eb2f11475e8c5b190a51b9a3938cb23d11a2f5b9
LinuxChata <linuxchata@hotmail.com>
2017-01-20 23:17:30 +0100

    Binary Search Trees

--- c#/W4.BinarySearchTrees/W4.BinarySearchTrees/Program.cs
+++ c#/W4.BinarySearchTrees/W4.BinarySearchTrees/Program.cs
@@ -6,6 +6,32 @@ namespace W4.BinarySearchTrees
     {
         public static void Main(string[] args)
         {
+            var bst = new BinarySearchTree<string, int>();
+            bst.Put("S", 47);
+            bst.Put("X", 14);
+            bst.Put("E", 32);
+            bst.Put("A", 16);
+            bst.Put("R", 61);
+            bst.Put("C", 89);
+            bst.Put("H", 17);
+            bst.Put("M", 67);
+
+            Console.WriteLine(bst.Get("H"));
+            Console.WriteLine(bst.Get("X"));
+            Console.WriteLine(bst.Get("Z"));
+
+            bst.Floor("G");
+
+            Console.WriteLine(bst.Rank("R"));
+
+            var keys = bst.Keys();
+
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            bst.DeleteMin();
 
             Console.ReadKey();
         }
