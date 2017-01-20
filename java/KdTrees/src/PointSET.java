7686ef88c67aea9ec7a86801b8d6bd008fcdbc65
LinuxChata <linuxchata@hotmail.com>
2017-01-20 23:26:03 +0100

    K-d trees: Format base code

--- java/KdTrees/src/PointSET.java
+++ java/KdTrees/src/PointSET.java
@@ -5,21 +5,55 @@
  *
  *  Compilation:  javac PointSET.java
  *  Execution:    java PointSET
- *  Dependencies: None
+ *  Dependencies: Point2D
  *
  *  Point data type
  *
  ******************************************************************************/
 
-public class PointSET {
-    public         PointSET()                               // construct an empty set of points
-    public           boolean isEmpty()                      // is the set empty?
-    public               int size()                         // number of points in the set
-    public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-    public           boolean contains(Point2D p)            // does the set contain point p?
-    public              void draw()                         // draw all points to standard draw
-    public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle
-    public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 
-    public static void main(String[] args)                  // unit testing of the methods (optional)
+public class PointSET {
+
+    // Construct an empty set of points
+    public PointSET() {
+    }
+
+    // Is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // Number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // Add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+    }
+
+    // Does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // Draw all points to standard draw
+    public void draw() {
+    }
+
+    // All points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // A nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return new Point2D(0, 0);
+    }
+
+    // Unit testing of the methods (optional)
+    public static void main(String[] args) {
+    }
 }
