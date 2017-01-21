34a37fe9de281cd12e71ebe79409ccb73a8c1c0c
LinuxChata <linuxchata@hotmail.com>
2017-01-21 11:56:40 +0100

    Geometric Applications

---
+++ c#/W5.GeometricApplications/W5.GeometricApplications/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W5.GeometricApplications
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
