57a2cdd38c577b453c4ee6e1ca56e84975aec54b
LinuxChata <linuxchata@hotmail.com>
2017-01-23 21:54:35 +0100

    KdTrees: Implement brute force algorithm. Scope 51/100

--- java/KdTrees/src/KdTree.java
+++ java/KdTrees/src/KdTree.java
@@ -3,8 +3,8 @@
  *  Written:       20/01/2017
  *  Last updated:  23/01/2017
  *
- *  Compilation:  javac PointSET.java
- *  Execution:    java PointSET
+ *  Compilation:  javac KdTree.java
+ *  Execution:    java KdTree
  *  Dependencies: Point2D
  *
  *  Point data type
@@ -13,28 +13,24 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 
-public class PointSET {
-    private TreeSet<Point2D> tree;
+public class KdTree {
 
     // Construct an empty set of points
-    public PointSET() {
-        tree = new TreeSet<Point2D>();
+    public KdTree() {
     }
 
     // Is the set empty?
     public boolean isEmpty() {
-        return this.size() == 0;
+        return false;
     }
 
     // Number of points in the set
     public int size() {
-        return tree.size();
+        return 0;
     }
 
     // Add the point to the set (if it is not already in the set)
@@ -42,8 +38,6 @@ public class PointSET {
         if (p == null) {
             throw new NullPointerException("Point cannot be null");
         }
-
-        this.tree.add(p);
     }
 
     // Does the set contain point p?
@@ -52,14 +46,11 @@ public class PointSET {
             throw new NullPointerException("Point cannot be null");
         }
 
-        return this.tree.contains(p);
+        return false;
     }
 
     // Draw all points to standard draw
     public void draw() {
-        for (Point2D point : this.tree) {
-            StdDraw.point(point.x(), point.y());
-        }
     }
 
     // All points that are inside the rectangle
@@ -69,12 +60,6 @@ public class PointSET {
         }
 
         List<Point2D> result = new ArrayList<Point2D>();
-        for (Point2D point : this.tree) {
-            if (rect.contains(point)) {
-                result.add(point);
-            }
-        }
-
         return result;
     }
 
@@ -84,25 +69,7 @@ public class PointSET {
             throw new NullPointerException("Point cannot be null");
         }
 
-        Point2D nearestPoint = null;
-        double minDistance = Math.sqrt(2);
-        for (Point2D point : this.tree) {
-            double distance = this.distance(p, point);
-            if (distance < minDistance) {
-                minDistance = distance;
-                nearestPoint = point;
-            }
-
-        }
-
-        return nearestPoint;
-    }
-
-    private double distance(Point2D p1, Point2D p2) {
-        double dx = Math.abs(p1.x() - p2.x());
-        double dy = Math.abs(p1.y() - p2.y());
-        double distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-        return distance;
+        return new Point2D(0, 0);
     }
 
     // Unit testing of the methods (optional)
