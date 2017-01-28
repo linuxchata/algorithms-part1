c269e22304a94bef07fc7dd67a66b5cf0e0b43d0
LinuxChata <linuxchata@hotmail.com>
2017-01-28 20:59:34 +0100

    Hash Tables: Implement Linear Probing

---
+++ c#/W6.HashTables/W6.HashTables/LinearProbing.cs
@@ -0,0 +1,56 @@
+﻿namespace W6.HashTables
+{
+    public class LinearProbing<TKey, TValue>
+    {
+        private readonly int m;
+
+        private readonly TKey[] keys;
+
+        private readonly TValue[] values;
+
+        public LinearProbing(int capacity)
+        {
+            this.m = capacity * 2;
+
+            this.keys = new TKey[this.m];
+            this.values = new TValue[this.m];
+        }
+
+        public TValue Get(TKey key)
+        {
+            var h = this.GetHashFunction(key.GetHashCode());
+
+            for (var i = h; this.keys[i] != null; i = (i + 1) % this.m)
+            {
+                if (this.keys[i].Equals(key))
+                {
+                    return this.values[i];
+                }
+            }
+
+            return default(TValue);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var h = this.GetHashFunction(key.GetHashCode());
+
+            int i;
+            for (i = h; this.keys[i] != null; i = i % this.m)
+            {
+                if (this.keys[i].Equals(key))
+                {
+                    break;
+                }
+            }
+
+            this.keys[i] = key;
+            this.values[i] = value;
+        }
+
+        private int GetHashFunction(int hashCode)
+        {
+            return (hashCode & 0x7fffffff) % this.m;
+        }
+    }
+}
