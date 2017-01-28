006399623474d917ea3d31c45384e2406a5fd2e2
LinuxChata <linuxchata@hotmail.com>
2017-01-28 21:04:52 +0100

    Hash Tables: Project changes and testing of Linear Probing

--- c#/W6.HashTables/W6.HashTables/Program.cs
+++ c#/W6.HashTables/W6.HashTables/Program.cs
@@ -17,6 +17,17 @@ namespace W6.HashTables
 
             hasTable.Get("A");
 
+            var hasTable2 = new LinearProbing<string, int>(10);
+            hasTable2.Add("S", 1);
+            hasTable2.Add("K", 0);
+            hasTable2.Add("R", 1);
+            hasTable2.Add("S", 5);
+            hasTable2.Add("A", 7);
+            hasTable2.Add("C", 9);
+            hasTable2.Add("Q", 3);
+
+            hasTable2.Get("A");
+
             Console.ReadLine();
         }
     }
