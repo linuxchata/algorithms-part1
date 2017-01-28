e1f0d2d0dbd26341434ac0010a9f5dc34b60ef90
LinuxChata <linuxchata@hotmail.com>
2017-01-28 23:17:35 +0100

    Sparse Vestors implementation

--- c#/W6.HashTables/W6.HashTables/Program.cs
+++ c#/W6.HashTables/W6.HashTables/Program.cs
@@ -28,6 +28,34 @@ namespace W6.HashTables
 
             hasTable2.Get("A");
 
+            var vector = new SparseVector[5];
+
+            vector[0] = new SparseVector();
+            vector[0].Put(1, 0.9);
+
+            vector[1] = new SparseVector();
+            vector[1].Put(2, 0.36);
+            vector[1].Put(3, 0.36);
+            vector[1].Put(4, 0.18);
+
+            vector[2] = new SparseVector();
+            vector[2].Put(3, 0.9);
+
+            vector[3] = new SparseVector();
+            vector[3].Put(0, 0.9);
+
+            vector[4] = new SparseVector();
+            vector[4].Put(0, 0.47);
+            vector[4].Put(2, 0.47);
+
+            var a = new[] { 0.05, 0.04, 0.36, 0.37, 0.19 };
+            var b = new double[a.Length];
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                b[i] = vector[i].Dot(a);
+            }
+
             Console.ReadLine();
         }
     }
