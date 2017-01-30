efe111e2268814eea009ccadb2e2133b3e07cb31
LinuxChata <linuxchata@hotmail.com>
2017-01-30 19:21:17 +0100

    Sort: Implement bubble sort

---
+++ c#/W3.BubbleSort/W3.BubbleSort/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace W3.BubbleSort
+{
+    public static class BubbleSort<T>
+    {
+        public static void Sort(T[] array, Comparer<T> c = null)
+        {
+            var comparer = c ?? Comparer<T>.Default;
+            for (var j = 1; j < array.Length; j++)
+            {
+                for (var i = 0; i < array.Length - j; i++)
+                {
+                    var r = comparer.Compare(array[i], array[i + 1]);
+                    if (r > 0)
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(T[] array, int i, int j)
+        {
+            var t = array[i];
+            array[i] = array[j];
+            array[j] = t;
+        }
+    }
+}
---
+++ c#/W3.BubbleSort/W3.BubbleSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace W3.BubbleSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t = new[] { 5, 1, 4, 2, 8 };
+            BubbleSort<int>.Sort(t);
+
+            Console.ReadLine();
+        }
+    }
+}
