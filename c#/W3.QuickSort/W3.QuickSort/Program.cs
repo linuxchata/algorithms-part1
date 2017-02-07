9f298235371f87409e9750995d571ece79d8dd1f
LinuxChata <linuxchata@hotmail.com>
2017-02-07 22:26:02 +0100

    Quick Sort: Implement 3 way partition

--- c#/W3.QuickSort/W3.QuickSort/Program.cs
+++ c#/W3.QuickSort/W3.QuickSort/Program.cs
@@ -8,12 +8,14 @@ namespace W3.QuickSort
         {
             var sort = "QUICKSORTEXAMPLE";
             var array = sort.ToCharArray();
-
             QuickSort<char>.Sort(array);
 
             var selectArray = sort.ToCharArray();
-            
             Console.Write(Selection<char>.Select(selectArray, 15));
+
+            var duplicateSort = "PABXWPPVPDPCYZ";
+            var arrayToSort = duplicateSort.ToCharArray();
+            ThreeWayPartition<char>.Sort(arrayToSort);
 
             Console.ReadKey();
         }
---
+++ c#/W3.QuickSort/W3.QuickSort/ThreeWayPartition.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace W3.QuickSort
+{
+    public static class ThreeWayPartition<T>
+    {
+        public static void Sort(T[] array, Comparer<T> comparer = null)
+        {
+            var c = comparer ?? Comparer<T>.Default;
+
+            var lo = 0;
+            var hi = array.Length - 1;
+            Partition(array, lo, hi, c);
+        }
+
+        private static void Partition(T[] array, int lo, int hi, Comparer<T> comparer)
+        {
+            if (lo > hi)
+            {
+                return;
+            }
+
+            var v = array[lo];
+            var i = lo;
+            var lt = lo;
+            var gt = hi;
+
+            while (i <= gt)
+            {
+                var c = comparer.Compare(array[i], v);
+                if (c < 0)
+                {
+                    Exchange(array, lt++, i++);
+                }
+                else if (c > 0)
+                {
+                    Exchange(array, gt--, i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            Partition(array, lo, lt - 1, comparer);
+            Partition(array, gt + 1, hi, comparer);
+        }
+
+        private static void Exchange(T[] array, int i, int j)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
